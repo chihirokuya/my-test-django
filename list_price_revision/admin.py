43db06dd873d117e5e9097f9bb8daf1368d1c491
chihiro <formybuisiness28@gmail.com>
2022-02-23 07:18:07 +0100

    色々

--- list_price_revision/admin.py
+++ list_price_revision/admin.py
@@ -4,8 +4,12 @@ from django.contrib import admin
 from .models import UserModel, AsinModel, RecordsModel, ListingModel, Q10ItemsLink, Q10BrandCode, LogModel
 
 
+class AsinAdmin(admin.ModelAdmin):
+    search_fields = ['asin']
+
+
 admin.site.register(UserModel)
-admin.site.register(AsinModel)
+admin.site.register(AsinModel, AsinAdmin)
 admin.site.register(RecordsModel)
 admin.site.register(ListingModel)
 admin.site.register(Q10ItemsLink)
