98e7a5496aaa351872561584c1987655457d96c9
chihiro <formybuisiness28@gmail.com>
2022-02-24 07:34:58 +0100

    色々

--- list_price_revision/views.py
+++ list_price_revision/views.py
@@ -567,7 +567,7 @@ def sell_and_not_stock(request):
     all_objects = AsinModel.objects.values('price', 'asin', 'product_name', 'description', 'product_group', 'brand')
     try:
         for elm in chunked(all_objects):
-            print('here')
+            print('here' )
             if elm['asin'] in asin_list:
                 if elm['price'] == 0 or is_in_black(elm)[0]:
                     no_stock_num += 1
