899b90c55322e55e84408b8c0fd45e6d1032ded2
chihirokuya <formybuisiness28@gmail.com>
2022-03-22 17:43:30 +0100

    色々

--- list_price_revision/api.py
+++ list_price_revision/api.py
@@ -1432,21 +1432,18 @@ def update_black_status():
 
 
 def update_selling_status(username):
-    print('here')
     # try:
     list_obj = ListingModel.objects.get(username=username)
     user_obj = UserModel.objects.get(username=username)
     # except:
     #     return
 
-    print(list_obj.asin_waiting_list)
-
     asin_list = list_obj.asin_list.split(',')
+    print(asin_list)
     selling_list = []
     no_stock_list = []
 
     for asin in asin_list:
-        print(asin)
         try:
             asin_obj = AsinModel.objects.get(asin=asin)
         except:
